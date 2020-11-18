fb8f2ad8c479aedace7b33a290863cde44266f71
takahiro.kawaguchi <Kawaguchi0402@icloud.com>
2020-11-18 13:57:16 +0000

    yatto

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,50 +1,50 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tasks = Task.all
+    @tasks=Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
     @task = Task.new
   end
-
+  
   def create
     @task = Task.new(task_params)
-
+    
     if @task.save
-      flash[:success] = 'タスクが正常に追加されました'
+      flash[:success] = 'タスクが正常に登録されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'タスクが追加されませんでした'
+      flash.now[:danger] = 'タスクが登録されませんでした'
       render :new
     end
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
-
+    
     if @task.update(task_params)
-      flash[:success] = 'タスクは正常に更新されました'
+      flash[:success] = 'タスクは正常に登録されました'
       redirect_to @task
     else
-      flash.now[:danger] = 'タスクは更新されませんでした'
+      flash.now[:danger] = 'タスクは登録されませんでした'
       render :edit
     end
   end
 
-  def destroy
-  end
-  
   private
-  
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content,:status)
   end
 end
