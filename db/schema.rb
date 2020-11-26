45e2843922c1f4036dea99e2976f5b2d776ee517
takahiro.kawaguchi <Kawaguchi0402@icloud.com>
2020-11-26 07:11:57 +0000

    touroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_031537) do
+ActiveRecord::Schema.define(version: 2020_11_26_061637) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
